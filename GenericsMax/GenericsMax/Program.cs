019cd76202fded3cfa09f203a34e8e3c0c28eccc
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-08-23 12:56:07 -0700

    Added MaxFloat

--- GenericsMax/GenericsMax/Program.cs
+++ GenericsMax/GenericsMax/Program.cs
@@ -8,12 +8,19 @@ namespace GenericsMax
         {
             Console.WriteLine("Welcome to Maximum program ");
             Console.WriteLine("Enter 1 to find Maximum of three integers ");
+            Console.WriteLine("Enter 2 to find Maximum of three float numbers ");
             switch(Convert.ToInt32(Console.ReadLine()))
                 {
                 case 1:
                    int intResult =MaxInteger.MaximumInt(35, 50, 60);
                     Console.WriteLine("Maximum value is = " + intResult);
                     break;
+                case 2:
+                    double floatResult = MaxFloat.MaximumFloat(5.5,7.8,25.6);
+                    Console.WriteLine("Maximum value is = " + floatResult);
+                    break;
+
+
                 default:
                     Console.WriteLine("Enter Valid Number");
                     break;
