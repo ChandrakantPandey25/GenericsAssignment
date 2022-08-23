47fb4800e9021ce28096ccc1c26e11cf84e6f901
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-08-23 12:13:57 -0700

    initial commit

---
+++ GenericsMax/GenericsMax/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericsMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
